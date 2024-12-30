5ba5b10164de2c08b5eed5965a73a255727f407b
Stephen Mitchell <stephen.p.mitchell@gmail.com>
2024-12-30 11:39:52 +0000

    Implemented PaymentCardExpiry in PaymentCard

--- src/Objects/DataTransferObjects/PaymentCard.php
+++ src/Objects/DataTransferObjects/PaymentCard.php
@@ -4,17 +4,18 @@ declare(strict_types=1);
 
 namespace AwesomeObjects\Objects\DataTransferObjects;
 
+use AwesomeObjects\Objects\ValueObjects\PaymentCardExpiry;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardHolder;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardNumber;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardSortCode;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardVerification;
-use InvalidArgumentException;
 
 final class PaymentCard
 {
     public function __construct(
         public readonly PaymentCardNumber $cardNumber,
         public readonly PaymentCardSortCode $sortCode,
+        public readonly PaymentCardExpiry $expiry,
         public readonly PaymentCardVerification $verificationNumber,
         public readonly PaymentCardHolder $cardHolder
     ) {
--- tests/PaymentCardTest.php
+++ tests/PaymentCardTest.php
@@ -4,6 +4,7 @@ namespace AwesomeObjects\Test;
 
 use AwesomeObjects\Enums\PaymentCardType;
 use AwesomeObjects\Objects\DataTransferObjects\PaymentCard;
+use AwesomeObjects\Objects\ValueObjects\PaymentCardExpiry;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardHolder;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardNumber;
 use AwesomeObjects\Objects\ValueObjects\PaymentCardSortCode;
@@ -14,9 +15,15 @@ class PaymentCardTest extends TestCase
 {
     public function testPaymentCard_ValidDetails_ReturnsPaymentCard(): void
     {
+        $date = (new \DateTimeImmutable('now'))
+            ->modify('+2 years');
+        $month = $date->format('m');
+        $year = substr($date->format('Y'), -2);
+
         $paymentCard = new PaymentCard(
             cardNumber: new PaymentCardNumber('4444333322221111'),
             sortCode: new PaymentCardSortCode('12-34-56'),
+            expiry: new PaymentCardExpiry($month, $year),
             verificationNumber: new PaymentCardVerification(
                 '123',
                 PaymentCardType::VISA
@@ -27,6 +34,7 @@ class PaymentCardTest extends TestCase
         $this->assertInstanceOf(PaymentCard::class, $paymentCard);
         $this->assertEquals('4444333322221111', $paymentCard->cardNumber);
         $this->assertEquals('12-34-56', $paymentCard->sortCode);
+        $this->assertEquals("$month/$year", $paymentCard->expiry);
         $this->assertEquals('123', $paymentCard->verificationNumber);
         $this->assertEquals('Mr Testy McTestface', $paymentCard->cardHolder);
     }
